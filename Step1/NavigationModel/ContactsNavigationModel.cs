5e8179b6f824a94af22a5ccb663514aa51124e4a
Michael L Perry <mperry@mallardsoft.com>
2011-03-02 21:05:47 -0600

    Set design width of person control.

---
+++ Step1/NavigationModel/ContactsNavigationModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using Hands_On_MVVM.Model;
+
+namespace Hands_On_MVVM.NavigationModel
+{
+    public class ContactsNavigationModel : INotifyPropertyChanged
+    {
+        private Person _selectedPerson;
+
+        public Person SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                _selectedPerson = value;
+                FirePropertyChanged("SelectedPerson");
+            }
+        }
+
+        private void FirePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Step1/ViewModel/CommandObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Hands_On_MVVM.ViewModel
+{
+    public class CommandObject : ICommand
+    {
+        private Action _action;
+
+        public CommandObject(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
